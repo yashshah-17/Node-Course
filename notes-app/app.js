2ea30da6d11793f8f2fcd9890bcc633bf9c876a6
Yash Shah <shahyash1707@gmail.com>
2020-04-15 20:55:07 -0300

    Taking input from user using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,50 @@
 const chalk = require('chalk')
+const validator = require('validator')
+const yargs = require('yargs')
 
-console.log(chalk.green("Success") + chalk.green("!"))
+yargs.command({
+    command: 'add',
+    describe: 'Adding a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'String'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'String'
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: " + argv.title)
+        console.log("Body: " + argv.body)
+    }
+})
+
+yargs.command({
+    command: 'delete',
+    describe: 'Deleting a note',
+    handler: function() {
+        console.log("Deleting a note")
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function() {
+        console.log("This is the list of all the notes")
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read all the notes',
+    handler:  function() {
+        console.log("Allowing to read all the notes")
+    }
+})
+
+yargs.parse()
